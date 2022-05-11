3e8ee320bca15aeeb8cf681788f06171c81f3ec5
Shaik Mohammad Sohail <mohammadsohailyt@gmail.com>
2022-05-11 19:21:19 +0530

    commit

--- merge sort.py
+++ merge sort.py
@@ -1,5 +1,24 @@
-def jamun():
-     pass
-def kosksk():
-     return 0
+def merge(left,right,small,big):
+     result=[]
+     small=[]
+     big=[]
+     i,j=0
+     while i<len(left) and j<len(right):
+          if left[i]<=right[j]:
+               small.append(left[i])
+               i+=1
+          else:
+               big.append(right[j])
+               j+=1
+     result=[small]+[big]
+     return result
+
+def merge(a):
+     mid=int(len(a)/2)
+     left=merge()
+      
+
+a=[1,4,2,3,5]
+
+print(a)
 
